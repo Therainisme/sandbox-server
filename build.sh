1507bc5fc1769921a57f8f4cc7e485825ae862fb
therainisme <therainisme@qq.com>
2022-04-29 19:42:38 +0800

    change build conf

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --rm -t therainisme/sandbox-server:2.0 .
+docker build --rm -t therainisme/sandbox-server:2.1 .
 docker rmi $(docker images | grep "none" | awk '{print $3}')
